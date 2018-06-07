707e913a1822353be7a06246351169e57b6d4301
unknown <parkhj0694@naver.com>
2018-06-07 10:04:56 +0900

    default constructor modified

--- src/MyMessage.java
+++ src/MyMessage.java
@@ -1,9 +1,12 @@
 public class MyMessage {
 	String message;
 	public MyMessage(String message) {
-		this.message = message;
+		this.message = "no message";
 	}
 	public void print() {
 		System.out.println(message);
+	}
+	public MyMessage(String message) {
+		this.message = message;
 	}
 }
